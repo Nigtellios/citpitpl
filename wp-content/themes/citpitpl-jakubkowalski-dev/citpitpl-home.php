34552ed74c0698e25f3dfa8d97ee4e732f1ff975
Jakubinka <nigtellios@gmail.com>
2020-12-08 15:34:27 +0100

    v0.3 - Menu Created

---
+++ wp-content/themes/citpitpl-jakubkowalski-dev/citpitpl-home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: CITPITPL Home
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package CitPitPL_-_jakubkowalski.dev
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+			the_content();
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
