1c487bb05ca57abed4340b033477ac33455c5982
Jakubinka <nigtellios@gmail.com>
2020-12-10 15:25:51 +0100

    v0.07

--- wp-content/themes/citpitpl-jakubkowalski-dev/js/custom.js
+++ wp-content/themes/citpitpl-jakubkowalski-dev/js/custom.js
@@ -1,10 +1,8 @@
 function openNav() {
-  // document.getElementById("overlay").style.width = "50%";
   document.getElementById("overlay").classList.add('opened');
 }
 
 function closeNav() {
-  // document.getElementById("overlay").style.width = "0%";
   document.getElementById("overlay").classList.remove('opened');
 }
 
