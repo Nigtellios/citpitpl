972d6152c74457a58d0caaf0c19a3a3237bb05d2
Jakubinka <nigtellios@gmail.com>
2020-12-16 18:16:34 +0100

    v1.0 - finished all sections
    Done: 
    + some rwd reworks
    + finished all sections
    Planned:
    + ACF PRO

---
+++ wp-content/themes/citpitpl-jakubkowalski-dev/container-template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Container Template JK
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package CitPitPL_-_jakubkowalski.dev
+ */
+
+get_header();
+?>
+
+	<main id="main" class="container site-main rules-padding">
+		<?php
+			the_content();
+		?>
+
+		
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
